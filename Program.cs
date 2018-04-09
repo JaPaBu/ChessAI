80fec1af29c732935c84ff486a1ac648c700b2f0
Jan <japa.busse@gmail.com>
2018-04-09 20:19:30 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# PongAI
-
-Code taken from https://medium.freecodecamp.org/simple-chess-ai-step-by-step-1d55a9266977
